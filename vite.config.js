1604173b2c251f334811a2b015308a2fa6a337ce
salehe rezapour <r.saleheh12@gmail.com>
2026-07-10 22:19:51 +0330

    changing vite.config.js file

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
  plugins: [react(), tailwindcss()],
- base: '/social-media-react-vite/',
+//  base: '/social-media-react-vite/',
 })
